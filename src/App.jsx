c97545205348d05d873f632f4f1dade68af83179
afzalhossainraju100 <raju23105341031@diu.edu.bd>
2025-10-31 13:35:36 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,74 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>React Core Concept</h1>
+      <Person />
+      <Students />
+      <Car />
+      <Device name="iPhone" price="$999" />
+      <Device name="Samsung Galaxy" price="$899" />
+      <Player name="Tamim" age="40" country="Bangladesh" runs="5000" />
+      <Player name="Shakib" age="35" country="Bangladesh" runs="4500" />
+      <Player name="Mushfiq" age="38" country="Bangladesh" runs="4000" />
+    </>
+  )
+}
+function Device(info){
+  return (
+    <div style={{border: '2px solid blue', margin: '20px', padding: '20px'}}>
+      <h2>This is a device user component</h2>
+      <p>Device Name: {info.name}</p>
+      <p>Device Price: {info.price}</p>
+    </div>
+  )
+}
+//const {Name, Age, Country, Runs} = {Name:"Tamim", Age:"40", Country:"Bangladesh", Runs:"5000"}
+function Player({name, age, country, runs}){
+  return (
+    <div style={{border: '2px solid green', margin: '20px', padding: '20px'}}>
+      <h2>This is a player component</h2>
+      <h3>Name:{name}</h3>
+      <p>Age:{age}</p>
+      <p>Country:{country}</p>
+      <p>Runs:{runs}</p>
+    </div>
+  )
+}
+
+function Person(){
+  const age = 25;
+  return (
+    <p>hello Mr Afal Hossaing Raju, you are {age} years old!</p>
+  )
+}
+function Students(){
+  const name = "Raju";
+  const age = 25;
+  return (
+    <div>
+      <h1>This is a student component</h1>
+      <p>hello Mr {name}, you are {age} years old, welcome to our website!</p>
+    </div>
+  )
+}
+function Car(){
+  return (
+    <div>
+      <h2>This is a car component</h2>
+      <div>
+        <img src="./assets/react.svg" alt="Car" />
+        <img src="./assets/react.svg" alt="Car" />
+        <img src="./assets/react.svg" alt="Car" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
